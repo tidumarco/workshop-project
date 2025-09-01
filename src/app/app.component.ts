758ff56b4ff39687252f945cce389be832ade61f
Marco Tidu <tidumarco@gmail.com>
2025-09-01 20:09:53 +0200

    Fixing image thumbnail logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import {Component, OnInit} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {NgForOf} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 import {ServiceOrder} from './models/ServiceOrder';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, NgForOf],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   serviceOrders: ServiceOrder[] = [];
+
+  title: string = 'Workshop Project';
 
   constructor(private http: HttpClient) {}
 
